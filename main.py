1e79f048bc06f071d605be9e406b69f548f349a8
Cesar Leon <alejandrorey2654@gmail.com>
2024-05-03 14:22:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from os import getenv
 app = Flask(__name__)
 app.secret_key = getenv('FLASK_KEY')
 
-coming_soon_active = True
+coming_soon_active = False
 
 @app.context_processor
 def inject_datetime():
